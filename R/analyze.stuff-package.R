a46a25886bca273e5aa686eae3d1b663ed0a7539
ejanalyst <info@ejanalysis.com>
2015-03-14 12:13:44 -0400

    Clean up package files like DESCRIPTION

--- R/analyze.stuff-package.R
+++ R/analyze.stuff-package.R
@@ -10,26 +10,24 @@
 #' Key functions include
 #' \cr
 #' \itemize{
-#' \item \code{\link{mem}}
-#' \item \code{\link{change.fieldnames}}
-#' \item \code{\link{calc.fields}}
-#' \item \code{\link{similar.p}}
-#' \item \code{\link{setdiff2}}
-#' \item \code{\link{pct.above}}
-#' \item \code{\link{cols.above.pct}}
-#' \item \code{\link{rowMaxs}}
-#' \item \code{\link{colMaxs}}
-#' \item \code{\link{wtd.rowMeans}}
-#' \item \code{\link{wtd.colMeans}}
-#' \item \code{\link{wtd.pctiles}}
-#' \item \code{\link{na.check}}
-#' \item \code{\link{pctiles}}
-#' \item \code{\link{length2}}
-#' \item \code{\link{dir2}}
+#' \item \code{\link{change.fieldnames}}: Change many fieldnames using map of current to new ones
+#' \item \code{\link{calc.fields}}: Create many new calculated fields from data.frame fields by specifying a list of formulas
+#' \item \code{\link{similar.p}}, \code{\link{setdiff2}}: Compare two datasets or sets
+#' \item \code{\link{count.above}}, \code{\link{pct.above}}: How many rows have values above a cutoff
+#' \item \code{\link{cols.above.count}}, \code{\link{cols.above.pct}}: How many cols have values above a cutoff
+#' \item \code{\link{rowMaxs}}, \code{\link{colMaxs}}, \code{\link{rowMins}}, \code{\link{colMins}}: Max or min of each row or col in data.frame or matrix
+#' \item \code{\link{wtd.rowMeans}}, \code{\link{wtd.colMeans}}: Weighted mean of each row or col
+#' \item \code{\link{pctiles}}, \code{\link{wtd.pctiles}}: See a table of values at 100 percentiles, for each field.
+#' \item \code{\link{na.check}}, \code{\link{length2}}: How many NA or non-NA values in each column
+#' \item \code{\link{mem}}: What objects are taking up the most memory
+#' \item \code{\link{dir2}}, \code{\link{dirr}}, \code{\link{dirdirs}}: Directory listing with wildcards, etc.
 #' }
-#' @author info@@ejanalysis.com<info@@ejanalysis.com>
+#' @author info@@ejanalysis.com <info@@ejanalysis.com>
 #'
 #' @references
 #'
 #' \url{http://www.ejanalysis.com}\cr
 #'
+#' **Acknowledgements:
+#' The useful package \pkg{sp} \code{\link[sp]{spDists}} \link{matrixStats} package will provide the basis for extended versions of rowMins, rowMax, colMins, colMaxs functions to be made available through this package.
+#' Source: Henrik Bengtsson (2015). matrixStats: Methods that Apply to Rows and Columns of a Matrix. R package version 0.13.1-9000. \url{https://github.com/HenrikBengtsson/matrixStats}

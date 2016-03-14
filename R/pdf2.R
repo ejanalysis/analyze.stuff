bdadfb2abbf9af3cb3ec6bfe541124236a8591eb
ejanalysis <info@ejanalysis.com>
2016-03-14 12:06:05 -0400

    pdf2 created to do quick overlay of 2 histograms

--- R/pdf2.R
+++ R/pdf2.R
@@ -3,13 +3,13 @@
 #' @param x Variable for histogram
 #' @param binx Variable that defines subsets
 #' @param threshold Variable that defines cutoff, so binx<threshold is plotted first, in one color, then binx>=threshold in other color is drawn over that
-#' @param colors Character vector length 2, with colors for first and second histogram 
+#' @param colors Character vector length 2, with colors for first and second histogram
 #' @param ... Other named parameters sent to hist, such as main, xlab, ylab
 #' @return Just plots using hist.
-#' @example  
+#' @examples
 #'   \dontrun{
 #'   pdf2(log10(bg$proximity.npl), bg$bin.EJ.DISPARITY.proximity.npl.eo, 10, main ='npl for high top 10 pct vs low EJ', xlab='NPL score log10')
-#'   } 
+#'   }
 #' @export
 pdf2 <- function(x, binx, threshold, n=100, colors=c('gray', 'red'), ...) {
   hist(x[ binx < threshold], n, col = colors[1], freq = TRUE, ...)

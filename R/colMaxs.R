2daa2286e493e5f0c08e0d129ef7ae51ab4980ef
ejanalyst <info@ejanalysis.com>
2022-08-29 00:17:04 -0400

    rowMaxs and colMaxs changedd

--- R/colMaxs.R
+++ R/colMaxs.R
@@ -8,15 +8,58 @@
 #' @template maxmin
 #' @export
 colMaxs <- function(df, na.rm=TRUE) {
+  answers <- vector(mode = 'numeric', length = NCOL(df))
+  numeric_ones <- sapply(df, is.numeric)
+
   if (is.matrix(df)) {
-    return( apply(df, 2, function(x) {
-      max(x, na.rm=na.rm)
-    } ) )
+    answers[numeric_ones]  <- matrixStats::colMaxs(df[ , numeric_ones,drop=FALSE])
+    answers[!numeric_ones] <- apply(df[ , !numeric_ones,drop=FALSE], 2, function(x) {max(x, na.rm=na.rm)})
   } else {
     # converts factors storing numbers to numbers, and factors storing character strings to character, which max likes better
     df <- factor.as.numeric(df, stringsAsFactors = FALSE )
-    return( sapply(df,   function(x) {
-      max(x, na.rm=na.rm)
-    } ) )
+    answers[numeric_ones]  <- matrixStats::colMaxs(as.matrix(df[ , numeric_ones,drop=FALSE]))
+    answers[!numeric_ones] <- apply(df[ , !numeric_ones,drop=FALSE], 2, function(x) {max(x, na.rm=na.rm)})
+    return(answers)
   }
 }
+
+
+#
+# n <- 10^5
+# testdf <- data.frame(a=1:n, b=sample(c(TRUE,FALSE),n,replace = T), c=sample(letters,n,replace = T), factor(sample(letters,n,replace = T)), stringsAsFactors = FALSE)
+# testdf <- cbind(testdf, testdf)
+# testdf <- cbind(testdf, testdf)
+# testdf <- cbind(testdf, testdf)
+# testdf <- cbind(testdf, testdf) # 64 columns
+# testdf <- cbind(testdf, testdf) # 128 columns
+# dim(testdf)
+
+# numeric_ones <- sapply(testdf, is.numeric)
+#  library(analyze.stuff) # then source new version
+
+# library(microbenchmark)
+# microbenchmark(colMaxs(testdf))
+
+# library(profvis) # requires latest R version
+ # profvis({print(colMaxs(testdf))})
+#
+# system.time(
+#   print(
+#     colMaxs(testdf)
+#   ))
+#
+# system.time(
+#   print(
+#   analyze.stuff::colMaxs(testdf) # old version
+# ))
+#
+# ######
+# system.time( # takes 3x the time of matrixStats version, to do just the numeric cols
+#   { apply(testdf[, numeric_ones, drop=FALSE], 2, function(x) {
+#     max(x, na.rm=TRUE)
+#   } )
+#   })
+# system.time(
+#   print(
+#     matrixStats::colMaxs(as.matrix(testdf[ , numeric_ones,drop=FALSE]))
+#   ))
--- R/rowMaxs.R
+++ R/rowMaxs.R
@@ -2,6 +2,14 @@
 #'
 #' @description
 #' Returns maximum value of each row of a data.frame or matrix.
+#'   pmax
+#' @details This uses do.call(pmax.int). pmax and pmin take one or more vectors as arguments,
+#'   recycle them to common length and return a single vector giving
+#'   the ‘parallel’ maxima (or minima) of the argument vectors.
+#'   pmax.int and pmin.int are faster internal versions only used when all arguments are atomic vectors and there are no classes: they drop all attributes.
+#'   (Note that pmax and pmax.int both fail for raw and complex vectors since these have no ordering.)
+#'   matrixStats::rowMaxs works for matrix only, not data.frame.
+#'
 #' @param df Data.frame or matrix, required.
 #' @param na.rm Logical value, optional, TRUE by default. Defines whether NA values should be removed first. Otherwise result will be NA when any NA is in the given vector.
 #' @return Returns a vector of numbers of length equal to number of rows in df.
@@ -17,7 +25,7 @@ rowMaxs <- function(df, na.rm=TRUE) {
   # if (!any(valid.cols)) {return(NA)}
   if (any(!valid.cols) ) {warning('using only numeric (double or integer) or logical or character columns -- ignoring other columns ')}
 
-  result <- do.call(pmax, c(df[ , valid.cols, drop = FALSE], na.rm=na.rm))
+  result <- do.call(pmax.int, c(df[ , valid.cols, drop = FALSE], na.rm=na.rm))
 
   result[nononmissing <- rowSums(!is.na(df[ , valid.cols, drop = FALSE]))==0] <- -Inf
   if (any(nononmissing)) {warning('where no non-missing arguments, returning -Inf')}

4357c732a69b1b53ba8bd67c8f010a1d291065e2
Mark Corrales <corrales.mark@epa.gov>
2022-12-13 14:05:33 -0500

    overlaps() can draw Venn diagram, and fixed overlaps count for nonunique

--- R/overlaps.R
+++ R/overlaps.R
@@ -4,6 +4,12 @@
 #'  to count how many are in each set, how many are in a not b, in b not a, in both, etc.
 #' @param a Required vector, such as list of FIPS character codes.
 #' @param b Required vector
+#' @param ab_names optional vector of 2 names to use as labels if drawing Venn
+#' @param ab_colors optional vector of 2 colors if drawing Venn
+#' @param venn_draw optional, whether to plot the Venn in viewer window
+#' @param venn_save optional, whether to save a png of the Venn plot
+#' @param filename optional, name of png file to use if venn_save is TRUE
+#' @param ... optional, passed to png() if venn_save is TRUE
 #' @param values Default is FALSE. If TRUE, output is logical data.frame with
 #'   union of only the unique elements as rownames, indicating which of those meet each criterion.
 #' @return Returns a data.frame of counts by default, formatted for viewing as a small table.
@@ -32,7 +38,9 @@
 #'
 #'  }
 #' @export
-overlaps <- function(a, b, values = FALSE) {
+overlaps <- function(a, b, values = FALSE,
+                     ab_names=c('a','b'), ab_colors=c('gray', 'orange'),
+                     venn_draw=TRUE, venn_save=FALSE, filename='venn.png', ...) {
 
   #     a and b, intersect(a,b) gives uniques
   #     a not b  setdiff(a,b) gives uniques
@@ -42,42 +50,59 @@ overlaps <- function(a, b, values = FALSE) {
   a.in.b <- a %in% b
   b.in.a <- b %in% a
 
-  total.in.a.only <- sum(!a.in.b)
-  total.in.b.only <- sum(!b.in.a)
+  total.in.a.only <- sum(!a.in.b) # not just unique a
+  total.in.b.only <- sum(!b.in.a) # not just unique b
 
   unique.in.a.only <- length(setdiff(a,b)) # should be same as length( setdiff(a,b) )
   unique.in.b.only <- length(setdiff(b,a)) # should be same as length( setdiff(b,a) )
 
-  results <- data.frame( rbind(
-    unique = c(
-      in.a =  length(unique(a)),
-      in.b =  length(unique(b)),
-      in.a.only = unique.in.a.only,
-      in.b.only = unique.in.b.only,
-      in.one.only = unique.in.a.only + unique.in.b.only,
-      overlap = length(intersect(a,b)),
-      union = length(union(a,b))
-    ),
-    total = c(
-      in.a =  length(a),
-      in.b =  length(b),
-      in.a.only =   total.in.a.only,
-      in.b.only =   total.in.b.only,
-      in.one.only = total.in.a.only + total.in.b.only,
-      overlap = sum(a.in.b) + sum(b.in.a),
-      union = length(a) + length(b)
-    )
-  ), stringsAsFactors = FALSE)
+  if (!values | venn_draw | venn_save) {
+    results <- data.frame( rbind(
+      unique = c(
+        in.a =  length(unique(a)),
+        in.a.only = unique.in.a.only,
+        overlap = length(intersect(a,b)),
+        in.b.only = unique.in.b.only,
+        in.b =  length(unique(b)),
+        in.one.only = unique.in.a.only + unique.in.b.only,
+        union = length(union(a,b))
+      ),
+      total = c(
+        in.a =  length(a),
+        in.a.only =   total.in.a.only,
+        overlap = length(intersect(a,b)), # wrong: sum(a.in.b) + sum(b.in.a),
+        in.b.only =   total.in.b.only,
+        in.b =  length(b),
+        in.one.only = total.in.a.only + total.in.b.only,
+        union = length(a) + length(b)
+      )
+    ), stringsAsFactors = FALSE)
+  }
 
+  if (venn_draw) {
+    VennDiagram::draw.pairwise.venn(
+      area1 = results['unique', ]$in.a, area2 = results['unique', ]$in.b,
+      cross.area = results['unique', ]$overlap, category = ab_names,
+      fill = ab_colors
+    )
+  }
+  if (venn_save) {
+    png(filename = filename, ...)
+    VennDiagram::draw.pairwise.venn(
+      area1 = results['unique', ]$in.a, area2 = results['unique', ]$in.b,
+      cross.area = results['unique', ]$overlap, category = ab_names,
+      fill = ab_colors
+    )
+    dev.off()
+  }
   if (values) {
 
     # PROVIDE A LOGICAL data.frame OF ALL unique NAMES & WHERE EACH IS FOUND:
-
     x <- unique(c(a, b[!b.in.a])) # like unique(union(a,b)) but in a particular order, showing all unique a first.
     xina <- x %in% a
     xinb <- x %in% b
     results <- data.frame(
-      #name = x,
+      value = x,
       in.a =  xina,
       in.b =  xinb,
       in.a.only = xinaonly <- xina & !xinb,

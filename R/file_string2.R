7f90dae4c4f1f9dd7fff64039c048725f939c22e
Mark Corrales <corrales.mark@epa.gov>
2023-01-28 23:37:41 -0500

    Create file_string2.R

---
+++ R/file_string2.R
@@ -0,0 +1,20 @@
+#' Read a text file and concatenate (some or all) lines by '\n' 
+#' Based on xfun::file_string()
+#' @param file Path to a text file (should be encoded in UTF-8).
+#' @param n Number of lines to view (or all, if n is missing/not specified)
+#'
+#' @return A character string of text lines concatenated by '\n'.
+#' @export
+#'
+#' @examples 
+#'   # file_string2(system.file("DESCRIPTION", package = "xfun"))
+#'   # help("file_string", package = "xfun") 
+file_string2 <- function(file, n) {
+  # from xfun::file_string()
+  x <- read_utf8(file)
+  if (length(x)) {
+    if (missing(n)) {n <- length(x)} else {n <- min(n, length(x))}
+    x <- paste(x[1:n], collapse = "\n")
+  }
+  raw_string(x)
+}

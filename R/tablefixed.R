5b9299d20796bd898f4427d8b02d462d217b80e7
Mark Corrales <corrales.mark@epa.gov>
2023-09-05 22:17:49 -0400

    Update tablefixed.R

--- R/tablefixed.R
+++ R/tablefixed.R
@@ -1,13 +1,17 @@
-#' Table of counts of integer values zero through maxbin
+#' tablefixed - Table of counts of integer values zero through maxbin
 #' @description Like tabulate or table, sort of, but includes zero unlike tabulate,
 #'   and lets you ensure results include every integer 0 through maxbin,
-#'   so you can for example easily combine tables of counts where some
+#'   so you can, for example, easily combine tables of counts where some
 #'   did not include all integers.
-#' @details When using a dataset like EJScreen with 12 indicators of interest,
-#'   and counting how many of the 12 are above various cutpoints,
+#' @details There is likely a more efficient way to do this in some existing package,
+#'   but this is useful and fast enough.
+#'
+#'   When using a dataset like EJScreen with 13 indicators of interest,
+#'   and counting how many of the 13 are above various cutpoints,
 #'   there may be zero rows that have exactly 8 above some cutoff, for example.
+#'
 #'   This function makes it easier to combine those tables into a summary
-#'   where 0-12 are in each table while table() would only return integers
+#'   where 0-13 are in each table while table() would only return integers
 #'   that came up in a given case (for one cutoff).
 #' @param x vector of integers, like counts, that can include 0
 #' @param maxbin highest integer among x, or number of bins

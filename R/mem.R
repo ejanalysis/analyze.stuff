42badc53f54ca8bc6eef72b3ea667e6aa911da77
Mark Corrales <corrales.mark@epa.gov>
2024-01-20 09:14:13 -0500

    Update mem.R

--- R/mem.R
+++ R/mem.R
@@ -1,24 +1,26 @@
-#' @title See what is using up memory
+#' @title See what is using up memory but just for 1 environment
 #' @description
 #' See a list of the largest objects in memory, and how much RAM they are using up
 #' Uses [object.size()]  to return info on memory consumption for largest n objects
 #' @param n Numeric, default is 10. How many objects to show (e.g., top 10)
-#' @return Results in printing a list of objects and their sizes
+#' @param env The environment to check, defaulting to .GlobalEnv but
+#'   could be something like env = as.environment(3) or 3
+#' @return   prints a list of objects and their sizes
 #' @examples
 #' \dontrun{
-#'  mem()
+#' mem()
 #' mem(15)
-#'
-#' # draw pie chart
-#' pie(object.sizes(), main="Memory usage by object")
+#' mem(env = 2)
 #'
 #' # draw bar plot
-#' barplot(object.sizes(),
+#' y = mem()
+#' barplot(y,
 #'         main="Memory usage by object", ylab="Bytes", xlab="Variable name",
-#'         col=heat.colors(length(object.sizes())))
+#'         col=heat.colors(length(y)))
 #'
-#' # draw dot chart
-#' dotchart(object.sizes(), main="Memory usage by object", xlab="Bytes")
+#' # draw pie chart again
+#' pie(mem(), main = "Memory usage by object")
+#'
 #'
 #' ################################## #
 #' # memory.size() and memory.limit() and object.sizes() comparison:
@@ -41,23 +43,56 @@
 #' head(cbind(object.sizes()))
 #' }
 #' @export
-mem <- function(n=10) {
-  if (length(ls(envir=.GlobalEnv)) > 0) {
-    object.sizes <- function() {
+mem <- function(n=10, env = .GlobalEnv) {
+  if (!(is.environment(env))) {
+    if  (is.numeric(env) & length(env) == 1 & round(env, 0) == env & env > 0 & env < 11) {
+      env <- as.environment(env)
+    } else {
+      warning('env = ', env, ' is not an environment so defaulting to .GlobalEnv'); env = .GlobalEnv
+    }}
+  cat('also see ?ls() with defaults for use inside a function. ')
+  if (length(ls(name = env)) > 0) {
+
+    object.sizes <- function(env = .GlobalEnv) {
       return(round(
-        rev(sort(sapply(ls(envir=.GlobalEnv), function (object.name)
-        object.size(get(object.name)))))/1e6,
+        rev(sort(sapply(ls(name = env), function (object.name)
+          object.size(get(object.name, envir = env) ))))/1e6,
         3))
       # format(object.size(get(object.name)), units = "MB")
       # could be used now instead of round( /1e6,3).
     }
-    if(.Platform$OS.type == "windows") {
-      cat(paste('R is using', memory.size(), 'MB out of limit', memory.limit(), 'MB\n'))
-    }
-    cat("Objects using the most memory (in MB):\n")
-    ( head(cbind(MB=object.sizes()),n) )
 
+    if(.Platform$OS.type == "windows") {
+      cat(  "  memory.size() is no longer supported fyi\n")
+      # cat(paste('R is using', memory.size(), 'MB out of limit', memory.limit(), 'MB\n'))
+    }
+
+    cat("Objects using the most memory (in MB):\n")
+    print( head(cbind(MB=object.sizes(env = env)),n) )
+    # browser()
+    pie_all = object.sizes(env = env)
+    pie_top = pie_all[pie_all > sum(pie_all) * 0.05]
+
+    pie_small_shown = setdiff(pie_all, pie_top)
+    if (length(pie_small_shown) > 3) {
+      pie_small_shown <- pie_small_shown[1:3]
+        pie_other <- setdiff( setdiff(pie_all, pie_top), pie_small_shown)
+        pie_other = sum(pie_other)
+        names(pie_other) <- "other"
+    } else {
+      pie_other = NULL
+    }
+
+
+    names(pie_top)[which.max(pie_top)] <- paste0(names(pie_top)[which.max(pie_top)] , " (", pie_top[which.max(pie_top)], " MB)")
+    if (pie_other == 0){ pie_show = c(pie_top, pie_small_shown} else {
+      pie_show = c(pie_top, pie_small_shown, pie_other)}
+    pie(pie_show, main = "Memory usage within just the specified environment (not all memory)")
+    # invisible( object.sizes(env = env))
+    return("")
   } else {
-    cat('Nothing found in memory using ls(envir=.GlobalEnv)\n')
+    cat('Nothing found in memory using ls(name = ', env,')\n')
+    return("")
   }
 }
+
